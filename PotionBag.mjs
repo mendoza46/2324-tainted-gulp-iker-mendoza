091cf04f0227fdce33e1427c08b62bb9ee273d50
mendoza46 <iker.mendoza@ikasle.aeg.eus>
2023-09-25 08:59:17 +0200

    [DELETE] borrar funciones innecesarias

---
+++ PotionBag.mjs
@@ -0,0 +1,7 @@
+export class PotionBag {
+    constructor(potions){
+        this.potions = potions;
+    }
+
+    
+}
--- index.mjs
+++ index.mjs
@@ -5,26 +5,28 @@ import { Cauldron } from "./cauldron.mjs";
 const execute = async () => {
     try{
         const data = await getData();
-        showIngredients(data);
+        // console.log(data);
 
-        //Creamos los ingredientes
-        const ingredients = Ingredients.load(data);
+        // showIngredients(data);
 
-        //Creamos el caldero de pociones
-        const cauldron = new Cauldron(ingredients);
+        // //Creamos los ingredientes
+        // const ingredients = Ingredients.load(data);
 
-        //creamos pociones
-        const potion1 = cauldron.createPotion("Bear Claws", "Bee");
-        showPotion(potion1);
+        // //Creamos el caldero de pociones
+        // const cauldron = new Cauldron(ingredients);
 
-        const potion2 = cauldron.createPotion("Chicken's Egg", "Chaurus Eggs");
-        showPotion(potion2);
+        // //creamos pociones
+        // const potion1 = cauldron.createPotion("Bear Claws", "Bee");
+        // showPotion(potion1);
 
-        const potion3 = cauldron.createPotion("Chaurus Eggs", "Bleeding Crown");
-        showPotion(potion3);
+        // const potion2 = cauldron.createPotion("Chicken's Egg", "Chaurus Eggs");
+        // showPotion(potion2);
+
+        // const potion3 = cauldron.createPotion("Chaurus Eggs", "Bleeding Crown");
+        // showPotion(potion3);
         
-        const potion4 = cauldron.createPotion("Nightshade", "Ectoplasm");
-        showPotion(potion4);
+        // const potion4 = cauldron.createPotion("Nightshade", "Ectoplasm");
+        // showPotion(potion4);
 
     } catch (error) {
         console.log(error)
@@ -33,18 +35,3 @@ const execute = async () => {
 } 
 
 execute();
-
-const showIngredients = (data) => {
-    data.ingredients.forEach(element => {
-        console.log(`${element.name}: `)
-        console.log(element.effects)
-    });
-}
-
-function showPotion(potion) {
-    console.log(`${potion.name}`);
-    console.log(`Value:     ${potion.value}`);
-    console.log(`Weight:    ${potion.weight}`);
-    console.log(`Time:      ${potion.time}`);
-    console.log(`---------------------------`);
-}
